13f4da6d326118c23efa3fb74786dafb0eb49ed5
sklesse <stefan.klesse@wsl.ch>
2025-07-02 12:54:36 +0200

    Update universalPOWT.R
    I discovered that the new function can still return negative power estimates. In that case the function now performs log-transformation.

--- R/universalPOWT.R
+++ R/universalPOWT.R
@@ -58,10 +58,16 @@ universalPOWT<-function (rwl, rescale = FALSE, return.power=FALSE)
     Xt <- x
     X.nna <- which(!is.na(x))
     X <- na.omit(x)
-    X2 <- X^p
+    if(p>0 ) {
+      X2 <- X^p
     Xt[X.nna] <- X2
     Xt
-    
+    }
+    if(p<=0 ) {
+      X2 <- log(X)
+      Xt[X.nna] <- X2
+      Xt
+    }
   }
   
   p<-fit.lm(rwl)
